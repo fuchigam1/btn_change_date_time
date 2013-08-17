b57075a21a5182fda236b3ebf44e812712859449
arata <contact@materializing.net>
2013-08-18 00:13:59 +0900

    change JavaScript部分を外部ファイル化

---
+++ vendors/js/admin/change_date_time.js
@@ -0,0 +1,35 @@
+/**
+ * ChangeDateTime Script
+ */
+$(function () {
+		
+	$('#BoxChangeNowDateTime').insertAfter('#BlogPostPostsDateTime');
+	
+	$('#BtnNowDateTime').click(function() {
+		btnChangeDateTimeValueChengeHandler();
+	});
+});
+
+function btnChangeDateTimeValueChengeHandler() {
+	
+	$('#BlogPostPostsDateDate').val('');
+	$('#BlogPostPostsDateTime').val('');
+	$('#BlogPostPostsDate').val('');
+	
+	var today = new Date();
+	var todayYear = today.getFullYear();
+	var todayMonth = today.getMonth();
+	var todayDay = today.getDate();	
+	var todayHours = today.getHours();
+	var todayMinutes = today.getMinutes();
+	var todaySeconds = today.getSeconds();
+	
+	var todayX = new XDate(todayYear, todayMonth, todayDay, todayHours, todayMinutes, todaySeconds);
+	var todayDateString = todayX.toString('yyyy/MM/dd');	
+	var todayTimeString = todayX.toString('HH:mm:ss');
+	
+	$('#BlogPostPostsDateDate').val(todayDateString);
+	$('#BlogPostPostsDateTime').val(todayTimeString);
+	$('#BlogPostPostsDate').val(todayDateString + ' ' + todayTimeString);
+	
+}
--- views/elements/admin/btn_change_date_time_form.php
+++ views/elements/admin/btn_change_date_time_form.php
@@ -9,41 +9,8 @@
  * @license			MIT
  */
 ?>
-<script type="text/javascript">
-$(function () {
-		
-	$('#BoxChangeNowDateTime').insertAfter('#BlogPostPostsDateTime');
-	
-	$('#BtnNowDateTime').click(function() {
-		btnChangeDateTimeValueChengeHandler();
-	});
-});
-
-function btnChangeDateTimeValueChengeHandler() {
-	
-	$('#BlogPostPostsDateDate').val('');
-	$('#BlogPostPostsDateTime').val('');
-	$('#BlogPostPostsDate').val('');
-	
-	var today = new Date();
-	var todayYear = today.getFullYear();
-	var todayMonth = today.getMonth();
-	var todayDay = today.getDate();	
-	var todayHours = today.getHours();
-	var todayMinutes = today.getMinutes();
-	var todaySeconds = today.getSeconds();
-	
-	var todayX = new XDate(todayYear, todayMonth, todayDay, todayHours, todayMinutes, todaySeconds);
-	var todayDateString = todayX.toString('yyyy/MM/dd');	
-	var todayTimeString = todayX.toString('HH:mm:ss');
-	
-	$('#BlogPostPostsDateDate').val(todayDateString);
-	$('#BlogPostPostsDateTime').val(todayTimeString);
-	$('#BlogPostPostsDate').val(todayDateString + ' ' + todayTimeString);
-	
-}
-</script>
 <?php $bcBaser->js('/btn_change_date_time/js/admin/xdate') ?>
+<?php $bcBaser->js('/btn_change_date_time/js/admin/change_date_time') ?>
 <div id="BoxChangeNowDateTime" class="box-tolink">
 	<?php echo $bcForm->button('現在日時に変更', array('div' => false, 'class' => 'button', 'id' => 'BtnNowDateTime')) ?>
 </div>
